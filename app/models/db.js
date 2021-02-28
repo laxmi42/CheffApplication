2578b686ef8cf28fb67c1114884f08568d0d3607
laxmi42 <58331265+laxmi42@users.noreply.github.com>
2021-02-28 13:31:07 +0530

    dwe

--- app/models/db.js
+++ app/models/db.js
@@ -18,14 +18,6 @@ console.log(connection);
 
 
 
-connection.connect(function(err) {
-  if (err) {
-      console.error('Error connecting: ' + err.stack);
-      return;
-  }
-
-  console.log('Connected as id ' + connection.threadId);
-});
 
 connection.query('SELECT * FROM categories', function (error, results, fields) {
   if (error)
