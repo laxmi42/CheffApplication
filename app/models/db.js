84eee7f2c7a305d50eb921c5515185b2c741a5c5
laxmi42 <58331265+laxmi42@users.noreply.github.com>
2021-02-28 12:58:47 +0530

    e23

--- app/models/db.js
+++ app/models/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
 var connection = mysql.createPool({
-  url: dbConfig.URL,
+
   host: dbConfig.HOST,
   port: dbConfig.PORT,
   user: dbConfig.USER,
