b5100bcc92148565115064b9494b3e6a8a3ed822
laxmi42 <58331265+laxmi42@users.noreply.github.com>
2021-02-28 15:04:12 +0530

    des

--- app/models/db.js
+++ app/models/db.js
@@ -20,14 +20,6 @@ var connection = mysql.createConnection({
 
 
 
-connection.query('SELECT * FROM categories', function (error, results, fields) {
-  if (error)
-      throw error;
-
-  results.forEach(result => {
-      console.log(result);
-  });
-});
 
 
 
