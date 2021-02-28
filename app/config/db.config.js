79eb5e83432d16610911cc6a7850222c9fd03a92
laxmi42 <58331265+laxmi42@users.noreply.github.com>
2021-02-28 12:17:50 +0530

    vfedgv

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  URL:    "http://31.220.48.219:3030/vijaydeliveryservices.com/",
- 
+  URL:    "http://vijaydeliveryservices.com/",
+  HOST:"127.0.0.1",
+  PORT:"3306",
   USER: "u271274439_cheffapp",
   PASSWORD: "Root@123",
   DB: " u271274439_cheffapp"
--- app/models/db.js
+++ app/models/db.js
@@ -3,6 +3,8 @@ const dbConfig = require("../config/db.config.js");
 
 var connection = mysql.createPool({
   url: dbConfig.URL,
+  host: dbConfig.HOST,
+  port: dbConfig.PORT,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   db: dbConfig.DB,
